e1bad8b2d7682419d2f9b8794a6b418040f4ac9e
bismansahni <bismansahni@outlook.com>
2024-12-24 13:48:20 +0530

    addeed fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-// import Hero from "@/components/LandingPage/hero";
-// import LandingPage from "@/components/LandingPage/LandingPageHeader";
-
-
-// export const App = () => {
-//   return (
-//     <div>
-//       <LandingPage />
-//       <Hero />
-//     </div>
-//   );
-// }
-// export default App;
 
 
 
