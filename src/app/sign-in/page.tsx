689ec02df69f899b3a55bfa120adf6607efa2c52
bismansahni <bismansahni@outlook.com>
2024-12-29 20:51:28 +0530

    unform colors

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,17 +1,39 @@
+// import AuthHeader from "@/components/auth/AuthLogoHeader/logoheader";
+// import { LoginForm } from "@/components/login-form";
+
+
+// const SignInPage = () => {
+//   return (
+//     <>
+//     <AuthHeader/>
+//     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+//     <div className="w-full max-w-sm">
+       
+//         <LoginForm />
+//       </div>
+//     </div>
+//     </>
+//   );
+// };
+
+// export default SignInPage;
+
+
+
 import AuthHeader from "@/components/auth/AuthLogoHeader/logoheader";
 import { LoginForm } from "@/components/login-form";
-
 
 const SignInPage = () => {
   return (
     <>
-    <AuthHeader/>
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-    <div className="w-full max-w-sm">
-       
-        <LoginForm />
+      <div className="bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300/80 dark:bg-gray-900 dark:from-gray-800 dark:via-gray-900 dark:to-gray-800">
+        <AuthHeader />
+        <div className="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
+          <div className="w-full max-w-sm">
+            <LoginForm />
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 };
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,18 +1,41 @@
+// import AuthHeader from "@/components/auth/AuthLogoHeader/logoheader";
+// import { SignUpForm } from "@/components/signup-form";
+
+
+
+// const SignUpPage = () => {
+//   return (
+//     <>
+//     <div className="bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300/80 dark:bg-gray-900 dark:from-gray-800 dark:via-gray-900 dark:to-gray-800"></div>
+//     <AuthHeader/>
+//     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+//     <div className="w-full max-w-sm">
+       
+//         <SignUpForm />
+//       </div>
+//     </div>
+//     </>
+//   );
+// };
+
+// export default SignUpPage;
+
+
+
 import AuthHeader from "@/components/auth/AuthLogoHeader/logoheader";
 import { SignUpForm } from "@/components/signup-form";
-
-
 
 const SignUpPage = () => {
   return (
     <>
-    <AuthHeader/>
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-    <div className="w-full max-w-sm">
-       
-        <SignUpForm />
+      <div className="bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300/80 dark:bg-gray-900 dark:from-gray-800 dark:via-gray-900 dark:to-gray-800 min-h-screen">
+        <AuthHeader />
+        <div className="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
+          <div className="w-full max-w-sm">
+            <SignUpForm />
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 };
